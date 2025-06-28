073ddfc3c1b828be5091e119615f80d8fa6748b1
gyuseon <mn5592@naver.com>
2025-06-28 17:13:25 +0900

    Feat: 닉네임 검증 기능 구현 (#114)

--- src/main/java/com/ilta/solepli/domain/profile/controller/ProfileController.java
+++ src/main/java/com/ilta/solepli/domain/profile/controller/ProfileController.java
@@ -5,11 +5,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
 import com.ilta.solepli.domain.profile.dto.response.UserProfileResponse;
@@ -39,7 +39,8 @@ public class ProfileController {
       summary = "닉네임 검증 API",
       description = "닉네임을 검증하는 API입니다. 중복여부, 입력 불가능한 닉네임 포함 여부를 검증합니다.")
   @GetMapping("/validate/nickname")
-  public ResponseEntity<SuccessResponse<Boolean>> validateNickname(@NotNull String nickname) {
+  public ResponseEntity<SuccessResponse<Boolean>> validateNickname(
+      @RequestParam(required = true) String nickname) {
     Boolean isValidated = profileService.validateNickname(nickname);
 
     return ResponseEntity.ok().body(SuccessResponse.successWithData(isValidated));
