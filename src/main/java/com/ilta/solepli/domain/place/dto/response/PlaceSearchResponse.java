7e7fadcc572e9928b97749a709215cebab6134c2
gyuseon <mn5592@naver.com>
2025-07-05 15:52:52 +0900

    Refactor: 장소 검색 응답 DTO 필드 추가 (#147)

--- src/main/java/com/ilta/solepli/domain/place/dto/response/PlaceSearchResponse.java
+++ src/main/java/com/ilta/solepli/domain/place/dto/response/PlaceSearchResponse.java
@@ -3,4 +3,11 @@ package com.ilta.solepli.domain.place.dto.response;
 import lombok.Builder;
 
 @Builder
-public record PlaceSearchResponse(Long id, String category, String name, String address) {}
+public record PlaceSearchResponse(
+    Long id,
+    String name,
+    String category,
+    String detailedCategory,
+    String address,
+    Double latitude,
+    Double longitude) {}
